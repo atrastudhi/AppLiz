67722a8cdcd5d29818fd5aa2c270dd4635745823
Atras Tudhi <atras.r@gmail.com>
2018-11-21 17:24:16 +0700

    register-login-logout

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,13 @@
 const express = require('express')
+const UserController = require('../controllers/UserController')
 const Route = express.Router()
+
+Route.get('/register', UserController.registerForm)
+Route.post('/register', UserController.addUser)
+
+Route.get('/login', UserController.loginForm)
+Route.post('/login', UserController.login)
+
+Route.get('/logout', UserController.logout)
 
 module.exports = Route
